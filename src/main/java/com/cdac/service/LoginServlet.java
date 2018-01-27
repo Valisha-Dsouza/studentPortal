a3fb3d7ab76516d01126848fda98d0f0b5b165b9
valisha <valishadsouza93@gmail.com>
2018-01-27 07:21:31 +0530

    Login servlet added

---
+++ src/main/java/com/cdac/service/LoginServlet.java
@@ -0,0 +1,42 @@
+package com.cdac.service;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.cdac.dao.CourseDao;
+import com.cdac.model.Course;
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		CourseDao coursedao=new CourseDao();
+		List<Course> cd= coursedao.listCourse();
+		request.setAttribute("courselist",cd);
+		RequestDispatcher rd=request.getRequestDispatcher("login.jsp");
+		rd.forward(request, response);
+		}
+	}
+
+
